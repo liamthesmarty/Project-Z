548bbd384bf9b1f1f6db2f48587e6388805302b1
liamthesmarty <liameyyladno@gmail.com>
2025-05-13 17:44:25 +0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,15 +1,15 @@
-local x=LocalPlayer()
+local a=LocalPlayer()
 local b={}
-b.cfg={d=3500}
-local s=false
-local u=vgui.GetWorldPanel
-local function r()
-if s then return end
-s=true
+b.c={d=3500}
+local f=false
+local g=vgui.GetWorldPanel
+local function i()
+if f then return end
+f=true
 render.Clear(0,0,0,255,true,true)
 render.RenderView({
-origin=x:EyePos(),
-angles=x:EyeAngles(),
+origin=a:EyePos(),
+angles=a:EyeAngles(),
 x=0,
 y=0;
 w=ScrW(),
@@ -19,89 +19,89 @@ drawhud=true;
 drawmonitors=true;
 drawviewmodel=true
 })
-local m=u()
+local m=g()
 if IsValid(m) then m:SetPaintedManually(true) end
 timer.Simple(5, function()
-u():SetPaintedManually(false)
-s=false
+g():SetPaintedManually(false)
+f=false
 end)
 end
-render.Capture = function(m)
-r()
-local S=render.Capture(m)
-return S
+render.Capture = function(h)
+i()
+local j=render.Capture(h)
+return j
 end
 b.frame=vgui.CreateX("EditablePanel")
-local gh={"box","name","hp","wep","role","rank"}
+local k={"box","name","hp","wep","role","rank"}
 do
-local F=b.frame
-F:SetSize(150,150)
-F:SetPos(100,100)
-F:MakePopup()
-function F:Paint(w,h)surface.SetDrawColor(30,30,30)surface.DrawRect(0,0,w,h)end
-for i,key in ipairs(gh)do
-local chk=vgui.Create("DCheckBoxLabel",F)
-chk:SetPos(10,10+(i-1)*20)
-chk:SetText(key)
-chk:SetValue(b.cfg[key])
-chk:SizeToContents()
-function chk:OnChange(val)b.cfg[key]=val end
-function chk.Button:Paint(w,h)
+local l=b.frame
+l:SetSize(99,130)
+l:SetPos(100,100)
+l:MakePopup()
+function l:Paint(n,m)surface.SetDrawColor(30,30,30)surface.DrawRect(0,0,n,m)end
+for p,r in ipairs(k)do
+local t=vgui.Create("DCheckBoxLabel",l)
+t:SetPos(10,10+(p-1)*20)
+t:SetText(r)
+t:SetValue(b.c[r])
+t:SizeToContents()
+function t:OnChange(u)b.c[r]=u end
+function t.Button:Paint(n,m)
 surface.SetDrawColor(12,12,12)
-surface.DrawOutlinedRect(0,0,w,h,1)
+surface.DrawOutlinedRect(0,0,n,m,1)
 surface.SetDrawColor(21,21,21)
-surface.DrawRect(0,0,w,h)
-if self:GetChecked()then surface.SetDrawColor(99,99,99)surface.DrawRect(3,3,w-6,h-6)end
+surface.DrawRect(0,0,n,m)
+if self:GetChecked()then surface.SetDrawColor(99,99,99)surface.DrawRect(3,3,n-6,m-6)end
 end
 end
 end
-local function k()
-if input.IsKeyDown(74)and not kd then
+local function v()
+if input.IsKeyDown(9)and not w then
 if IsValid(b.frame)then b.frame:SetVisible(not b.frame:IsVisible())end
 end
-kd=input.IsKeyDown(74)
-if input.IsKeyDown(73)and not del then
+w=input.IsKeyDown(9)
+if input.IsKeyDown(73)and not x then
 if IsValid(b.frame)then b.frame:Remove()end
 hook.Remove("HUDPaint","DrawRecordingIcon")
 hook.Remove("Think","DecorProps")
 end
-del=input.IsKeyDown(73)
+x=input.IsKeyDown(73)
 end
-hook.Add("Think","DecorProps",k)
-local function u()
-local ply=player.GetAll()
-for i=1,#ply do
-local a=ply[i]
-if a==x or not a:Alive()or x:GetPos():DistToSqr(a:GetPos())>b.cfg.d^2 then continue end
-surface.SetAlphaMultiplier(a:IsDormant()and 0.4 or 1)
-local pos=a:GetPos()
-local min,max=a:OBBMins(),a:OBBMaxs()
-local pos2=(pos+Vector(min.x,0,max.z)):ToScreen()
-pos=pos:ToScreen()
-local h,w=pos.y-pos2.y,(pos.y-pos2.y)/2
-if b.cfg.name then draw.SimpleTextOutlined(a:Nick(),"default",pos.x,pos2.y-2,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
-if b.cfg.rank then draw.SimpleTextOutlined(a:GetUserGroup(),"default",pos.x,pos2.y-10,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
-if b.cfg.wep then
-local z=a:GetActiveWeapon()
-if IsValid(z)then draw.SimpleTextOutlined(z:GetPrintName(),"default",pos.x,pos.y+5,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
+hook.Add("Think","DecorProps",v)
+local function y()
+local z=player.GetAll()
+for i=1,#z do
+local j=z[i]
+if j==a or not j:Alive()or a:GetPos():DistToSqr(j:GetPos())>b.c.d^2 then continue end
+surface.SetAlphaMultiplier(j:IsDormant()and 0.4 or 1)
+local w=j:GetPos()
+local min,max=j:OBBMins(),j:OBBMaxs()
+local y=(w+Vector(min.x,0,max.z)):ToScreen()
+w=w:ToScreen()
+local h,n=w.y-y.y,(w.y-y.y)/2
+if b.c.name then draw.SimpleTextOutlined(j:Nick(),"DefaultSmall",w.x,y.y-2,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
+if b.c.rank then draw.SimpleTextOutlined(j:GetUserGroup(),"DefaultSmall",w.x,y.y-10,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
+if b.c.wep then
+local z=j:GetActiveWeapon()
+if IsValid(z)then draw.SimpleTextOutlined(z:GetPrintName(),"DefaultSmall",w.x,w.y+5,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
 end
-if b.cfg.role then draw.SimpleTextOutlined(team.GetName(a:Team()),"default",pos.x,pos.y+(b.cfg.wep and 16 or 5),color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
-if b.cfg.hp then
-local hp=math.Clamp(a:Health(),0,100)
-local hh=h/100*hp
-local x=pos.x-w/2-5
+if b.c.role then draw.SimpleTextOutlined(team.GetName(j:Team()),"DefaultSmall",w.x,w.y+(b.c.wep and 16 or 5),color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
+if b.c.hp then
+local hp=math.Clamp(j:Health(),0,100)
+local hh=n/100*hp
+local x=w.x-n/2-5
 surface.SetDrawColor(20,20,20)
-surface.DrawRect(x,pos2.y-1,w/w+2,h+2)
+surface.DrawRect(x,y.y-1,n/n+2,h+2)
 surface.SetDrawColor(HSVToColor(hp/100*120,1,1))
-surface.DrawRect(x+1,pos.y-(a:Health()>100 and h or hh),w/w,(a:Health()>100 and h or hh))
+surface.DrawRect(x+1,w.y-(j:Health()>100 and h or hh),n/n,(j:Health()>100 and h or hh))
 end
-if b.cfg.box then
-surface.SetDrawColor(team.GetColor(a:Team()))
-surface.DrawOutlinedRect(pos.x-w/2,pos2.y,w,h)
+if b.c.box then
+surface.SetDrawColor(team.GetColor(j:Team()))
+surface.DrawOutlinedRect(w.x-n/2,y.y,n,h)
 surface.SetDrawColor(0,0,0)
-surface.DrawOutlinedRect(pos.x-w/2-1,pos2.y-1,w+2,h+2)
-surface.DrawOutlinedRect(pos.x-w/2+1,pos2.y+1,w-2,h-2)
+surface.DrawOutlinedRect(w.x-n/2-1,y.y-1,n+2,h+2)
+surface.DrawOutlinedRect(w.x-n/2+1,y.y+1,n-2,h-2)
 end
 end
 end
-hook.Add("HUDPaint","DrawRecordingIcon",u)
+hook.Add("HUDPaint","DrawRecordingIcon",y)
