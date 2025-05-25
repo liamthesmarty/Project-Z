af9a28baa2dfcee439b82812384ececfcbff14fe
liamthesmarty <liameyyladno@gmail.com>
2025-05-25 15:10:14 +0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,3 +1,4 @@
+local a=LocalPlayer()
 local s=GetRenderTarget("g" .. os.time(),ScrW(),ScrH())
 local function l(vOrigin,vAngle,vFOV)
 local h={
@@ -20,20 +21,19 @@ hook.Run("HUDPaintZ")
 cam.End2D()
 render.SetRenderTarget(s)
 end
-hook.Add("RenderScene","RenderScene", function(vOrigin,vAngle,vFOV)
+hook.Add("RenderScene","b", function(vOrigin,vAngle,vFOV)
 l(vOrigin,vAngle,vFOV)
 end)
-hook.Add("ShutDown","ShutDown", function()
+hook.Add("ShutDown","n", function()
 render.SetRenderTarget()
 end)
-local a=LocalPlayer()
 local b={}
 b.c={d=3000}
 b.m=vgui.CreateX("EditablePanel")
-local k={"box","nick","hp","wep","role","rank"}
+local k={"box","nick","hp","wep","role"}
 do
 local l=b.m
-l:SetSize(99,130)
+l:SetSize(80,110)
 l:SetPos(100,100)
 function l:Paint(n,m)surface.SetDrawColor(30,30,30)surface.DrawRect(0,0,n,m)end
 for p,r in ipairs(k)do
@@ -53,33 +53,21 @@ end
 end
 end
 local function v()
-if input.IsKeyDown(10)and not w then
+if input.IsKeyDown(4)and not w then
 if IsValid(b.m)then b.m:SetVisible(not b.m:IsVisible())end
 end
-w=input.IsKeyDown(10)
+w=input.IsKeyDown(4)
 if input.IsKeyDown(73)and not x then
 if IsValid(b.m)then b.m:Remove()end
-hook.Remove("HUDPaint","DrawRecordingIcon")
+hook.Remove("DrawOverlay","b")
 hook.Remove("Think","a")
+hook.Remove("ShutDown","n")
+hook.Remove("RenderScene","b")
 end
 x=input.IsKeyDown(73)
 end
-if IsValid(a:GetActiveWeapon()) then
-local r=a:GetActiveWeapon()
-if r:GetClass():find("weapon_octo") then
-r.VisualRecoilHorizontal = 0
-r.VisualRecoilVertical = 0
-end
-end
 hook.Add("Think","a",v)
 local function y()
-local fps=math.floor(1/RealFrameTime())
-local c="lavahook | "..fps.." fps"
-surface.SetFont("DefaultSmall")
-local o,q=surface.GetTextSize(c)
-local p=5
-draw.RoundedBox(4,p,p,o+p*2,q+p*2,Color(78,122,107))
-draw.SimpleText(c,"DefaultSmall",p*2,p*2,color_white,TEXT_ALIGN_LEFT,TEXT_ALIGN_TOP)
 local z=player.GetAll()
 for i=1,#z do
 local j=z[i]
@@ -89,9 +77,8 @@ local w=j:GetPos()
 local u,v=j:OBBMins(),j:OBBMaxs()
 local y=(w+Vector(u.x,0,v.z)):ToScreen()
 w=w:ToScreen()
-local h,n=w.y-y.y,(w.y-y.y)/2
+local h,n=w.y-y.y,(w.y-y.y)/1.8
 if b.c.nick then draw.SimpleTextOutlined(j:Name(),"DefaultSmall",w.x,y.y-2,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
-if b.c.rank then draw.SimpleTextOutlined(j:GetUserGroup(),"DefaultSmall",w.x,y.y-10,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_BOTTOM,1,color_black)end
 if b.c.wep then
 local z=j:GetActiveWeapon()
 if IsValid(z)then draw.SimpleTextOutlined(z:GetPrintName(),"DefaultSmall",w.x,w.y+5,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
@@ -99,11 +86,11 @@ end
 if b.c.role then draw.SimpleTextOutlined(team.GetName(j:Team()),"DefaultSmall",w.x,w.y+(b.c.wep and 16 or 5),color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER,1,color_black)end
 if b.c.hp then
 local hp=math.Clamp(j:Health(),0,100)
-local hh=n/100*hp
+local hh=h/100*hp
 local x=w.x-n/2-4
 surface.SetDrawColor(20,20,20)
 surface.DrawRect(x,y.y-1,n/n+2,h+2)
-surface.SetDrawColor(HSVToColor(hp/100*120,1,1))
+surface.SetDrawColor(0,255,0)
 surface.DrawRect(x+1,w.y-(j:Health()>100 and h or hh),n/n,(j:Health()>100 and h or hh))
 end
 if b.c.box then
@@ -115,4 +102,4 @@ surface.DrawOutlinedRect(w.x-n/2+1,y.y+1,n-2,h-2)
 end
 end
 end
-hook.Add("HUDPaint","DrawRecordingIcon",y)
+hook.Add("DrawOverlay","b",y)
